87361838d36ec962458aa8f0f23f1a34f890f3d9
Elimane FOFANA <elimane.fofana@mediasoftci.com>
2019-06-24 19:54:02 +0000

    new

--- src/Engine.java
+++ src/Engine.java
@@ -1,4 +1,27 @@
 public class Engine {
 
-    
+    public static  String researcher(String keyword)
+    {
+        String kw = String.valueOf(keyword);
+        String res = (Wrapper.TypeConstants.NAME_PATTERN.matches(kw)) ? "name" : (Wrapper.TypeConstants.EMAIL_PATTERN.matches(kw)) ? "email" : (Wrapper.TypeConstants.PHONE_NUMBER.matches(kw)) ? "phone": "unknown";
+        switch(res) {
+            case "name":
+                // Statements
+                break; // optional
+
+            case "email" :
+                // Statements
+                break; // optional
+
+            case "phone" :
+                // Statements
+                break; // optional
+
+            // You can have any number of case statements.
+            default : // Optional
+                // Statements
+        }
+
+        return "";
+    }
 }
